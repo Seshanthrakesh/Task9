06cb75b1f0daddc8026f8cf682baec88248796b4
seshanth <seshanthrakesh1901@gmail.com>
2023-09-19 14:43:02 +0530

    Mistakes are updates with New answers

--- src/basics/Q2Reversestring.java
+++ src/basics/Q2Reversestring.java
@@ -6,12 +6,24 @@ public class Q2Reversestring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Enter String to be Reversed: ");
 		Scanner sc = new Scanner(System.in);
-		String a=sc.nextLine();
-		
-		System.out.println(a);
-		
+        // Read a string from the user
+        System.out.print("Enter a string: ");
+        String inputString = sc.nextLine();
+
+        // Initialize an empty string to store the reversed string
+        String reversedString = "";
+
+        // Iterate through the characters of the input string in reverse order
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversedString += inputString.charAt(i);
+        }
+
+        // Output the reversed string
+        System.out.println("Reversed string: " + reversedString);
+
+        // Close the scanner
+       sc.close();
 	}
 
 }
